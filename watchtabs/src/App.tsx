c05eb9af9e577e4314c79f473839a709ffd90678
jassem-razzaq <jassem.razzaq@gmail.com>
2025-10-07 21:01:27 -0400

    feat: use array to output list elements

--- watchtabs/src/App.tsx
+++ watchtabs/src/App.tsx
@@ -1,36 +1,51 @@
-import { useState } from "react";
+import { useState, type ReactElement } from "react";
 
 function Movie() {
-  return (
-    <div className="movie">
-      <div className="movie-cont">Movie 1</div>
-      <div className="movie-button-cont">
-        <button className="movie-button">
-          <i class="fa-regular fa-clone"></i>
-        </button>
-        <button className="movie-button">
-          <i class="fa-regular fa-trash-can"></i>
-        </button>
+  // Get list of movies from tabs in array
+  const movieArr: string[] = [
+    "The Matrix (1999)",
+    "The Shawshank Redemption (1999)",
+    "3 Idiots (2017)",
+    "The Matrix (1999)",
+    "The Shawshank Redemption (1999)",
+    "3 Idiots (2017)",
+    "The Matrix (1999)",
+    "The Shawshank Redemption (1999)",
+    "3 Idiots (2017)",
+    "The Matrix (1999)",
+    "The Shawshank Redemption (1999)",
+    "3 Idiots (2017)",
+    "The Matrix (1999)",
+    "The Shawshank Redemption (1999)",
+    "3 Idiots (2017)",
+  ];
+
+  // Create output array of list elements
+  let resultArr: ReactElement[] = [];
+
+  // Loop over movies array and create list elements
+  for (let i = 0; i < movieArr.length; i++) {
+    resultArr.push(
+      <div className="movie">
+        <div className="movie-cont">{movieArr[i]}</div>
+        <div className="movie-button-cont">
+          <button className="movie-button">
+            <i className="fa-regular fa-clone"></i>
+          </button>
+          <button className="movie-button">
+            <i className="fa-regular fa-trash-can"></i>
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return resultArr;
 }
 
 function MovieList() {
   return (
     <div className="movie-list">
       <ol className="list-element">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
         <Movie />
       </ol>
     </div>
